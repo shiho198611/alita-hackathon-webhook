6e57fb9a0ba88322572700d9d4a0ce890e5449bb
David.Huang <david.huang@skylinetw.com>
2019-02-18 17:54:21 +0800

    Update test.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+'use strict';
+
+const express = require('express');
+const http = require('https');
+var port = process.env.PORT || 8080;
+
+const server = express();
+
 server.get('/getName',function (req,res){
     res.send('Swarup Bam');
 });
