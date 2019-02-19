73c6a6a965397e05d10fe2b26cf1c846d119bd9d
David.Huang <david.huang@skylinetw.com>
2019-02-19 15:47:12 +0800

    Debug.

--- actionConst.js
+++ actionConst.js
@@ -1,5 +1,5 @@
 module.exports = {
-    actionAlitaAllowanceType = "alita_allowance_type",
-    actionTravelAllowance = "alita_travel_allowance",
-    alitaApiUrl = 'https://protected-lowlands-62741.herokuapp.com/api/alita'
+    actionAlitaAllowanceType: 'alita_allowance_type',
+    actionTravelAllowance: 'alita_travel_allowance',
+    alitaApiUrl: 'https://protected-lowlands-62741.herokuapp.com/api/alita'
 }
