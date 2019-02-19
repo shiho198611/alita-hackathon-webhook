6c33c04e86b6a81f2a7e34cfd55f0c38462d9b28
David.Huang <david.huang@skylinetw.com>
2019-02-19 10:13:54 +0800

    Test update.

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ server.post('/actalita', function(reqeust, response) {
     console.log('Act webhook test');
     console.log('request response id: '+reqeust.responseId);
     console.log('Req body: '+JSON.stringify(reqeust.body));
+    console.log('Res body: '+JSON.stringify(response.body));
     console.log('Get Act intent name: '+reqeust.body.queryResult.intent.name);
 });
 
