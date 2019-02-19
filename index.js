b7610231885bd7a377764140ac5d0c19d3e8e418
David.Huang <david.huang@skylinetw.com>
2019-02-19 10:39:23 +0800

    Test update.

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ server.get('/getName',function (req,res){
     res.send('Swarup Bam');
 });
 
-server.post('/actalita', function(reqeust, response) {
+server.post('/actalita', function(req, res) {
     console.log('Act webhook test');
-    console.log('request response id: '+reqeust.responseId);
-    console.log('Req body: '+JSON.stringify(reqeust.body));
-    console.log('Res body: '+JSON.stringify(response.body));
-    console.log('Get Act intent name: '+reqeust.body.queryResult.intent.name);
+    console.log('request response id: '+req.responseId);
+    
+    console.log('Req: '+req);
+    console.log('Res: '+res);
+
+    console.log('Req body: '+JSON.stringify(req.body));
+    console.log('Res body: '+JSON.stringify(res.body));
+    console.log('Get Act intent name: '+req.body.queryResult.intent.name);
 });
 
 server.listen(port, function () {
